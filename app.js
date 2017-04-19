a0c005f963998d77a220aaadaf97b484e3793506
rwt <rwt@rwtdeMacBook-Pro-2.local>
2017-04-19 13:53:40 +0800

    :smile:

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var session = require('express-session');
+var favicon = require('serve-favicon')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -17,8 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(express.favicon());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
