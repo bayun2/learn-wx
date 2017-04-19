0275ff7ba47bafb88925369a2b3a78ac89ff8981
rwt <rwt@rwtdeMacBook-Pro-2.local>
2017-04-19 11:46:51 +0800

    :smile:

--- routes/oauth.js
+++ routes/oauth.js
@@ -3,12 +3,16 @@ var router = express.Router();
 var OAuth = require('wechat-oauth');
 var client = new OAuth('wxfcd813854f6de23e', '834177ce77f63959c559c0cfeeceaaf7');
 
-router.get('/', function(req, res, next) {
+var getWxCode = function() {
   var domain = 'http://123.206.133.45';
-  var auth_callback_url = domain + '/oauth/callback/';
+  var auth_callback_url = domain + '/oauth/callback';
   var url = client.getAuthorizeURL(auth_callback_url, '', 'snsapi_userinfo');
   console.log(url);
   res.redirect(url);
+}
+
+router.get('/', function(req, res, next) {
+  getWxCode();
 });
 
 router.get('/callback', function(req, res, next) {
@@ -20,19 +24,21 @@ router.get('/callback', function(req, res, next) {
     var code = req.query.code;
     console.log(code);
     client.getAccessToken(code, function(err, result) {
-      console.log(err);
-      console.log(result);
-      var accessToken = result.data.access_token;
-      var openid = result.data.openid;
-      console.log('6')
-      client.getUser(openid, function(err, result) {
-        var userInfo = result;
-        console.log('7')
-        req.session.currentUser = userInfo;
-        console.log(req.session)
-        console.log('8')
-        res.render('oauth', { userInfo: userInfo });
-      })
+      try {
+        var accessToken = result.data.access_token;
+        var openid = result.data.openid;
+        console.log('6')
+        client.getUser(openid, function(err, result) {
+          var userInfo = result;
+          console.log('7')
+          req.session.currentUser = userInfo;
+          console.log(req.session)
+          console.log('8')
+          res.render('oauth', { userInfo: userInfo });
+        })
+      } catch(e) {
+        getWxCode();
+      }
     })
   }
 });
