fdff34563f58a0256bf372dd5ec907a296a9b2f1
rwt <rwt@rwtdeMacBook-Pro-2.local>
2017-04-19 15:33:08 +0800

    :smile:

--- routes/oauth.js
+++ routes/oauth.js
@@ -7,7 +7,6 @@ var getWxCode = function(res) {
   var domain = 'http://123.206.133.45';
   var auth_callback_url = domain + '/oauth/callback';
   var url = client.getAuthorizeURL(auth_callback_url, '', 'snsapi_userinfo');
-  console.log(url);
   res.redirect(url);
 }
 
@@ -20,24 +19,18 @@ router.get('/callback', function(req, res, next) {
   if (req.session.currentUser) {
     res.render('oauth', { userInfo: req.session.currentUser });
   } else {
-    console.log('~~~~')
     var code = req.query.code;
-    console.log(code);
     client.getAccessToken(code, function(err, result) {
       try {
         var accessToken = result.data.access_token;
         var openid = result.data.openid;
-        console.log('6')
         client.getUser(openid, function(err, result) {
           var userInfo = result;
-          console.log('7')
           req.session.currentUser = userInfo;
-          console.log(req.session)
-          console.log('8')
-          res.render('oauth', { userInfo: userInfo });
+          res.render('oauth', { userInfo: JSON.stringify(userInfo)});
         })
       } catch(e) {
-        
+        console.log(e)
       }
     })
   }
